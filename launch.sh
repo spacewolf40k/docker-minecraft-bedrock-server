cbfc5adbb4b4765228b6d5ab1130ee04965c9cc1
kai-lam <kai_lam@apple.com>
2020-07-08 14:16:46 -0700

    Update 2020-07-08_14:16:46

--- launch.sh
+++ launch.sh
@@ -1,3 +1,13 @@
 #/bin/bash
 
-docker run -d -it -e EULA=TRUE -e VERSION=1.16 -e DIFFICULTY=normal -e ALLOW_CHEATS=true -e GAMEMODE=creative -p 19132:19132/udp -v /Users/kai_lam/git/docker-minecraft-bedrock-server/server.properties:/data/server.properties itzg/minecraft-bedrock-server
+allowCheats="true" # true, false
+containerPort="19132"
+difficulty="normal" # normal, easy, hard
+gameMode="creative" # creative, survival
+hostPort="19132"
+bedrockVersion="1.16"
+
+serverProperties="/Users/kai_lam/git/docker-minecraft-bedrock-server/server.properties:/data/server.properties"
+permissions="/Users/kai_lam/git/docker-minecraft-bedrock-server/permissions.json:/data/permissions.json"
+
+docker run -d -it -e EULA=TRUE -e VERSION=${bedrockVersion} -e DIFFICULTY=${difficulty} -e ALLOW_CHEATS=${allowCheats} -e GAMEMODE=${gameMode} -p ${containerPort}:${hostPort}/udp -v ${serverProperties} -v ${permissions}  itzg/minecraft-bedrock-server
